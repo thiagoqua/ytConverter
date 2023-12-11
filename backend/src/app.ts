c2ae33dc2869faea65727490d7811b342c397d4d
tiki <thiagoqua16@gmail.com>
2023-12-11 11:34:29 -0300

    change cors rules from each api path.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,31 +2,34 @@ import express from "express";
 import { downloadRouter } from "./routes/download";
 import cors from "cors";
 import { wakeupRouter } from "./routes/wakeup";
+import cron from "node-cron";
+import { fetchWelcome } from "./utils/fetchWelcome";
 
 const app = express();
 const PORT = process.env.PORT ?? 8080;
-
-app.use(cors({
-  origin:(origin,callback) => {
+const corsRules = {
+  origin: (origin: any, callback: any) => {
     const ACCEPTED_ORIGINS = [
-      'http://localhost:4200',
-      'https://youtconverter.netlify.app',
-      'https://youtconverter.netlify.app/',
-      'https://wake-up-alarm.onrender.com'     //wake up alarm
-    ]
-    
-    if(origin && ACCEPTED_ORIGINS.includes(origin))
-      return callback(null,true);
+      "http://localhost",
+      "https://youtconverter.netlify.app",
+      "https://youtconverter.netlify.app/",
+    ];
 
-    return callback(new Error('NOT ALLOWED BY CORS'));
+    if (origin && ACCEPTED_ORIGINS.includes(origin))
+      return callback(null, true);
+
+    return callback(new Error("NOT ALLOWED BY CORS"));
   },
-  exposedHeaders: "Content-Disposition"
-}));
+  exposedHeaders: "Content-Disposition",
+};
 
 // paths
-app.use('/download',downloadRouter);
-app.use('/wakeup',wakeupRouter);
+app.use("/wakeup", cors(), wakeupRouter);
+app.use("/download",cors(corsRules), downloadRouter);
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
   console.log(`server started on http://localhost:${PORT}`);
 });
+
+//to wake up the app in intervals of 20 hours
+cron.schedule('0 * */20 * * *',fetchWelcome);
---
+++ backend/src/utils/fetchWelcome.ts
@@ -0,0 +1,3 @@
+export function fetchWelcome(){
+  fetch('http://localhost:8080/wakeup');
+}
